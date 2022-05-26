7370e22eb9980e30fc7062b9d6fd6c29b97d3d8e
LANGATG <langatgilbert1115@Gmail.com>
2022-05-26 11:39:57 +0300

    added report fields

--- EasyPro/Controllers/ProductIntakesController.cs
+++ EasyPro/Controllers/ProductIntakesController.cs
@@ -188,7 +188,7 @@ namespace EasyPro.Controllers
                 productIntake.SaccoCode = sacco ?? "";
                 productIntake.Description = "Intake";
                 productIntake.TransactionType = TransactionType.Intake;
-                //productIntake.TransDate = DateTime.Today;
+                productIntake.TransDate = DateTime.Today;
                 productIntake.TransTime = DateTime.UtcNow.AddHours(3).TimeOfDay;
                 productIntake.Balance = GetBalance(productIntake);
                 _context.ProductIntake.Add(productIntake);
@@ -308,7 +308,7 @@ namespace EasyPro.Controllers
             {
                 productIntake.Description = "Correction";
                 productIntake.TransactionType = TransactionType.Correction;
-                //productIntake.TransDate = DateTime.Today;
+                productIntake.TransDate = DateTime.Today;
                 productIntake.TransTime = DateTime.UtcNow.AddHours(3).TimeOfDay;
                 productIntake.Balance = GetBalance(productIntake);
                 _context.Add(productIntake);
