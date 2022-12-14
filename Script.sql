769b6bd97810c80ed0301d48181a9a3808278406
LANGATG <langatgilbert1115@Gmail.com>
2022-12-14 11:16:15 +0300

    tanykina updates on scripts

--- Script.sql
+++ Script.sql
@@ -1,125 +1,24 @@
 USE [MORINGA]
 GO
 
-CREATE TABLE [dbo].[DispatchBalancing](
-	[Id] [bigint] IDENTITY(1,1) NOT NULL,
-	[Date] [smalldatetime] NULL,
-	[Intake] [money] NULL,
-	[Dispatch] [money] NULL,
-	[CF] [money] NULL,
-	[BF] [money] NULL,
-	[Actuals] [money] NULL,
-	[Spillage] [money] NULL,
-	[Varriance] [money] NULL,
-	[Saccocode] [nvarchar](50) NULL
+CREATE TABLE [dbo].[d_PreSets](
+	[Id] [bigint] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
+	[SNo] [bigint] NULL,
+	[Deduction] [varchar](50) NULL,
+	[Remark] [varchar](150) NULL,
+	[StartDate] [varchar](50) NULL,
+	[Rate] [money] NULL,
+	[Stopped] [bit] NULL,
+	[Auditdatetime] [datetime] NULL,
+	[AuditId] [varchar](50) NULL,
+	[Rated] [bit] NULL,
+	[status] [bigint] NULL,
+	[status2] [bigint] NULL,
+	[status3] [bigint] NULL,
+	[status4] [bigint] NULL,
+	[status5] [bigint] NULL,
+	[status6] [bigint] NULL,
+	[BranchCode] [nvarchar](50) NULL,
+	[saccocode] [nvarchar](50) NULL
 ) ON [PRIMARY]
-
 GO
-
-CREATE TABLE ExcelDump (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[LoggedInUser] [varchar](50) NULL,
-	[SaccoCode] [varchar](50) NULL,
-	[Sno] [varchar](50) NULL,
-	[ProductType] [varchar](50) NULL,
-	[Quantity] [money] NULL,
-	[TransDate] datetime NULL
-);
-
-CREATE TABLE FLMD (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Sno] [varchar](50) NULL,
-	[ExoticCattle] [int] NULL,
-	[IndigenousCattle] [int] NULL,
-	[IndigenousChicken] [int] NULL,
-	[Sheep] [int] NULL,
-	[Goats] [int] NULL,
-	[Camels] [int] NULL,
-	[Donkeys] [int] NULL,
-	[Pigs] [int] NULL,
-	[BeeHives] [int] NULL,
-	[Boys] [int] NULL,
-	[Girls] [int] NULL,
-	[Deaths] [int] NULL,
-	[Disabled] [int] NULL,
-	[PostGraduates] [int] NULL,
-	[Graduates] [int] NULL,
-	[UnderGraduates] [int] NULL,
-	[Secondary] [int] NULL,
-	[Primary] [int] NULL,
-	[Nursery] [int] NULL,
-	[SaccoCode] [varchar](50) NULL,
-);
-
-CREATE TABLE FLMDCrops (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Sno] [varchar](50) NULL,
-	[CashCrops] [varchar](50) NULL,
-	[ConsumerCrops] [varchar](50) NULL,
-	[Vegetables] [varchar](50) NULL,
-	[AnimalFeeds] [varchar](50) NULL,
-	[SaccoCode] [varchar](50) NULL,
-);
-
-CREATE TABLE FLMDLand (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Sno] [varchar](50) NULL,
-	[Location] [varchar](50) NULL,
-	[PlotNumber] [varchar](50) NULL,
-	[TotalAcres] [float] NULL,
-	[AcresCrops] [float] NULL,
-	[AcresBuilding] [float] NULL,
-	[AcresLivestock] [float] NULL,
-	[acresUnusedLand] [float] NULL,
-	[SaccoCode] [varchar](50) NULL,
-);
-
-CREATE TABLE County (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Name] [varchar](50) NULL,
-	[Contact] [varchar](50) NULL,
-	[Closed] [bit] NOT NULL,
-	[CreatedOn] [smalldatetime] NULL,
-	[CreatedBy] [varchar](50) NULL,
-);
-
-CREATE TABLE SubCounty (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Name] [varchar](50) NULL,
-	[County] [varchar](50) NULL,
-	[Contact] [varchar](50) NULL,
-	[Closed] [bit] NOT NULL,
-	[CreatedOn] [smalldatetime] NULL,
-	[CreatedBy] [varchar](50) NULL,
-);
-
-CREATE TABLE Ward (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[Name] [varchar](50) NULL,
-	[SubCounty] [varchar](50) NULL,
-	[Contact] [varchar](50) NULL,
-	[Closed] [bit] NOT NULL,
-	[CreatedOn] [smalldatetime] NULL,
-	[CreatedBy] [varchar](50) NULL,
-);
-
-CREATE TABLE ProductIntake (
-	Id bigint IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	Sno nvarchar(50) NOT NULL,
-	TransDate datetime,
-	TransTime time(7),
-	ProductType nvarchar(50),
-	QSupplied money,
-	PPU money,
-	CR money,
-	DR money,
-	Balance money,
-	Description nvarchar(50),
-	Paid bit NOT NULL,
-	Remarks nvarchar(50),
-	TransactionType int,
-	AuditId nvarchar(50),
-	auditdatetime datetime,
-	Branch nvarchar(50),
-	SaccoCode nvarchar(50)
-);
