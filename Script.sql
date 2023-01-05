4349a45b32e21931134c167ea50386f2090098ec
Omwitsa <omwitsawilson@yahoo.com>
2023-01-05 14:41:35 +0300

    Save changes

--- Script.sql
+++ Script.sql
@@ -1,29 +1,18 @@
 USE [MORINGA]
 GO
 
-CREATE TABLE [dbo].[d_PreSets](
+CREATE TABLE [dbo].[d_SupplierCumlative](
 	[Id] [bigint] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
-	[SNo] [nvarchar](50) NULL,
-	[Deduction] [varchar](50) NULL,
-	[Remark] [varchar](150) NULL,
-	[StartDate] [varchar](50) NULL,
-	[Rate] [money] NULL,
-	[Stopped] [bit] NULL,
-	[Auditdatetime] [datetime] NULL,
-	[AuditId] [varchar](50) NULL,
-	[Rated] [bit] NULL,
-	[status] [bigint] NULL,
-	[status2] [bigint] NULL,
-	[status3] [bigint] NULL,
-	[status4] [bigint] NULL,
-	[status5] [bigint] NULL,
-	[status6] [bigint] NULL,
-	[BranchCode] [nvarchar](50) NULL,
-	[saccocode] [nvarchar](50) NULL
+	[SNo] [nvarchar](50) NOT NULL,
+	[Names] [nvarchar](50) NULL,
+	[Cummulative] [money] NULL,
+	[AuditId] [nvarchar](50) NULL,
+	[Date] [datetime] NULL,
+	[Branch] [nvarchar](50) NULL,
+	[SaccoCode] [nvarchar](50) NULL,
+	[ProductType] [nvarchar](50) NULL,
+ CONSTRAINT [PK_d_SupplierCumlative] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
-GO
-
-
-add Remarks, Branch to Dispatch table
-add column extension to dpayroll and dtransporterspayroll table
-add column Flmd to USERGROUPS
