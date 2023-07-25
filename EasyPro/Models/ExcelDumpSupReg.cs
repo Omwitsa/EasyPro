df42d4da691ef5d9c2c1ad7d3c10b14fd3146357
LANGATG <langatgilbert1115@Gmail.com>
2023-07-25 09:31:38 +0300

    suppliers upload design

---
+++ EasyPro/Models/ExcelDumpSupReg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EasyPro.Models
+{
+    public class ExcelDumpSupReg
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Reg_date { get; set; }
+        public string SNo { get; set; }
+        public string Names { get; set; }
+        public string PhoneNo { get; set; }
+        public string IdNo { get; set; }
+        public DateTime DOB { get; set; }
+        public string Acc_Number { get; set; }
+        public string Bank_code { get; set; }
+        public string Bank_Branch { get; set; }
+        public string Gender { get; set; }
+        public string PaymentMode { get; set; }
+        public string Village { get; set; }
+        public string LOCATION { get; set; }
+        public string WARD { get; set; }
+        public string SUB_COUNTY { get; set; }
+        public string COUNTY { get; set; }
+        public string LoggedInUser { get; set; }
+        public string Branch { get; set; }
+        public string SaccoCode { get; set; }
+    }
+}
--- EasyPro/Utils/Utilities.cs
+++ EasyPro/Utils/Utilities.cs
@@ -72,6 +72,95 @@ namespace EasyPro.Utils
             controller.ViewBag.isTanyakina = sacco == StrValues.Tanykina;
         }
 
+        public string GenerateExcelGridSupReg(ISheet sheet, string sacco, string loggedInUser, string branch)
+        {
+            StringBuilder sb = new StringBuilder();
+            IRow headerRow = sheet.GetRow(0); //Get Header Row
+            int cellCount = headerRow.LastCellNum;
+            sb.Append("<table class='table table-bordered'><tr>");
+            for (int j = 0; j < cellCount; j++)
+            {
+                NPOI.SS.UserModel.ICell cell = headerRow.GetCell(j);
+                if (cell == null || string.IsNullOrWhiteSpace(cell.ToString())) continue;
+                sb.Append("<th>" + cell.ToString() + "</th>");
+            }
+            sb.AppendLine("</tr>");
+            sb.Append("<tr>");
+            var existingData = _context.ExcelDumpSupReg.Where(d => d.LoggedInUser == loggedInUser && d.SaccoCode == sacco).ToList();
+            if(existingData.Any())
+                _context.ExcelDumpSupReg.RemoveRange(existingData);
+
+            decimal totalQnty = 0;
+            var excelDumps = new List<ExcelDumpSupReg>();
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                for (int j = row.FirstCellNum; j < cellCount; j++)
+                {
+                    if (row.GetCell(j) != null)
+                        sb.Append("<td>" + row.GetCell(j).ToString() + "</td>");
+                }
+                // Reg_date, SNo, Names, PhoneNo, IdNo, DOB, Acc_Number, Bank_code, Bank_Branch,
+                // Gender, PaymentMode, Village, LOCATION, WARD, SUB_COUNTY, COUNTY, LoggedInUser, Branch, SaccoCode
+                decimal.TryParse(row.GetCell(2).ToString(), out decimal qnty);
+                totalQnty += qnty;
+                String dateString = (row.GetCell(0)).ToString();
+                DateTime transDate = DateTime.Parse(dateString);
+
+                var DobdateString = row.GetCell(5).ToString();
+                DateTime DobtransDate = DateTime.Parse(DobdateString);
+
+                excelDumps.Add(new ExcelDumpSupReg
+                {
+                    LoggedInUser = loggedInUser,
+                    SaccoCode = sacco,
+                    Branch = branch,
+                    Reg_date = transDate,
+                    SNo = row.GetCell(1)?.ToString() ?? "",
+                    Names = row.GetCell(2)?.ToString() ?? "",
+                    PhoneNo = row.GetCell(3)?.ToString() ?? "",
+                    IdNo = row.GetCell(4)?.ToString() ?? "",
+                    DOB = DobtransDate,
+                    Acc_Number = row.GetCell(6)?.ToString() ?? "",
+                    Bank_code = row.GetCell(7)?.ToString() ?? "",
+                    Bank_Branch = row.GetCell(8)?.ToString() ?? "",
+                    Gender = row.GetCell(9)?.ToString() ?? "",
+                    PaymentMode = row.GetCell(10)?.ToString() ?? "",
+                    Village = row.GetCell(11)?.ToString() ?? "",
+                    LOCATION = row.GetCell(12)?.ToString() ?? "",
+                    WARD = row.GetCell(13)?.ToString() ?? "",
+                    SUB_COUNTY = row.GetCell(14)?.ToString() ?? "",
+                    COUNTY = row.GetCell(15)?.ToString() ?? "",
+                });
+                
+                sb.AppendLine("</tr>");
+            }
+
+            if (excelDumps.Any())
+            {
+                _context.ExcelDumpSupReg.AddRange(excelDumps);
+                _context.SaveChanges();
+            }
+
+
+            sb.Append("<tr>");
+            for (int j = 0; j < cellCount; j++)
+            {
+                if(j == 0)
+                    sb.Append("<td>Total</td>");
+                else if(j == 2)
+                    sb.Append("<td>" + totalQnty + "</td>");
+                else
+                    sb.Append("<td></td>");
+            }
+            
+            sb.AppendLine("</tr>");
+            sb.Append("</table>");
+
+            return sb.ToString();
+        }
         public string GenerateExcelGrid(ISheet sheet, string sacco, string loggedInUser, string branch)
         {
             StringBuilder sb = new StringBuilder();
@@ -87,7 +176,7 @@ namespace EasyPro.Utils
             sb.AppendLine("</tr>");
             sb.Append("<tr>");
             var existingData = _context.ExcelDump.Where(d => d.LoggedInUser == loggedInUser && d.SaccoCode == sacco).ToList();
-            if(existingData.Any())
+            if (existingData.Any())
                 _context.ExcelDump.RemoveRange(existingData);
 
             decimal totalQnty = 0;
@@ -118,7 +207,7 @@ namespace EasyPro.Utils
                     TransDate = transDate,
                     TransCode = row.GetCell(4)?.ToString() ?? ""
                 });
-                
+
                 sb.AppendLine("</tr>");
             }
 
@@ -132,20 +221,19 @@ namespace EasyPro.Utils
             sb.Append("<tr>");
             for (int j = 0; j < cellCount; j++)
             {
-                if(j == 0)
+                if (j == 0)
                     sb.Append("<td>Total</td>");
-                else if(j == 2)
+                else if (j == 2)
                     sb.Append("<td>" + totalQnty + "</td>");
                 else
                     sb.Append("<td></td>");
             }
-            
+
             sb.AppendLine("</tr>");
             sb.Append("</table>");
 
             return sb.ToString();
         }
-
         public decimal? GetBalance(ProductIntake productIntake)
         {
             var latestIntake = _context.ProductIntake.Where(i => i.Sno == productIntake.Sno && i.SaccoCode.ToUpper().Equals(productIntake.SaccoCode.ToUpper()))
