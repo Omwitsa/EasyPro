0bc4fa6f03f79c334272ab8e265f27eee4ced84d
LANGATG <langatgilbert1115@Gmail.com>
2023-08-21 09:30:43 +0300

    Done new account login to pick automatically

--- EasyPro/Models/Coffee/DeliveryNote.cs
+++ EasyPro/Models/Coffee/DeliveryNote.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
-namespace EasyPro.Models
+namespace EasyPro.Models.Coffee
 {
     public partial class DeliveryNote
     {
@@ -14,27 +14,13 @@ namespace EasyPro.Models
         public string NoteNo { get; set; }
         public string GrowersCode { get; set; }
         public string CoffeType { get; set; }
-        public double? Qin { get; set; }
-        public double? Qout { get; set; }
-        public DateTime? LastDUpdated { get; set; }
+        public string VehicleNo { get; set; }
+        public decimal? NoBags { get; set; }
+        public string Driver { get; set; }
+        public string Comment { get; set; }
         public string UserId { get; set; }
         public DateTime? AuditDate { get; set; }
-        public double? OBal { get; set; }
-        public string SupplierId { get; set; }
-        public string Serialized { get; set; }
-        public string Unserialized { get; set; }
-        public int? Seria { get; set; }
-        public decimal? Pprice { get; set; }
-        public decimal? Sprice { get; set; }
         public string Branch { get; set; }
-        public string Draccno { get; set; }
-        public string Craccno { get; set; }
-        public int? Ai { get; set; }
-        public DateTime? Expirydate { get; set; }
-        public long? Run { get; set; }
-        public int? Process1 { get; set; }
-        public int? Process2 { get; set; }
-        public string Remarks { get; set; }
         public string saccocode { get; set; }
     }
 }
---
+++ EasyPro/Models/Coffee/MarketingDeduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#nullable disable
+
+namespace EasyPro.Models.Coffee
+{
+    public partial class MarketingDeduct
+    {
+        [Key]
+        public long Id { get; set; }
+        public string saccocode { get; set; }
+        public string Factory { get; set; }
+        public string Remarks { get; set; }
+        public string one { get; set; }
+        public string second { get; set; }
+        public decimal Amount { get; set; }
+        public virtual List<Marketing> Marketing { get; set; }= new List<Marketing>();
+        public DateTime AuditDateTime { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
